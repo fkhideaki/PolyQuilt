6a0c9c1b3c9fab74ad68f005f0392ca219191091
fkhd <fkhideaki@gmail.com>
2022-12-02 23:04:40 +0900

    UI描画効率改善

--- Addons/PolyQuilt/pq_keymap_editor.py
+++ Addons/PolyQuilt/pq_keymap_editor.py
@@ -25,73 +25,45 @@ def draw_tool_keymap( layout ,keyconfing,keymapname ) :
     layout.context_pointer_set('keymap', keymap)
     cnt = 0
 
-
     for item in reversed(keymap.keymap_items) :
-        cnt = max( cnt , (item.oskey,item.shift,item.ctrl,item.alt).count(True) )
+        cnt = max(cnt, (item.oskey, item.shift, item.ctrl, item.alt).count(True) )
 
-    for item in reversed(keymap.keymap_items) :
-        if True in (item.oskey,item.shift,item.ctrl,item.alt) :
-            it = layout.row( )
-#            it.prop(item , "active" , text = "" )
-            if item.idname == 'mesh.poly_quilt' :
-#               for i3d in keyconfing.keymaps["Mesh"].keymap_items :
-#                    if i3d.type == 'LEFTMOUSE' and i3d.shift == item.shift and i3d.ctrl == item.ctrl and i3d.alt == item.alt and i3d.oskey == item.oskey :
-#                        ic = layout.row(align = True)
-#                        ic.template_event_from_keymap_item(i3d)
-#                        ic.label( icon = 'ERROR' , text = i3d.name  )
-#                for i3d in keyconfing.keymaps["3D View"].keymap_items :
-#                    if i3d.type == 'LEFTMOUSE' and i3d.shift == item.shift and i3d.ctrl == item.ctrl and i3d.alt == item.alt and i3d.oskey == item.oskey :
-#                        ic = layout.row(align = True)
-#                        ic.template_event_from_keymap_item(i3d)
-#                        ic.label( icon = 'ERROR' , text = i3d.name  )
+    for item in reversed(keymap.keymap_items):
+        if not (True in (item.oskey, item.shift, item.ctrl, item.alt)):
+            continue
+        it = layout.row()
+        if item.idname != 'mesh.poly_quilt':
+            continue
 
-#               ic = it.row(align = True)
-#               ic.prop( item ,   icon = 'ERROR' )
+        ic = it.row(align = True)
+        ic.ui_units_x = cnt + 2
+        ic.prop(item , "active" , text = "" , emboss = True )
+        ic.template_event_from_keymap_item(item)
 
-                ic = it.row(align = True)
-                ic.ui_units_x = cnt + 2
-                ic.prop(item , "active" , text = "" , emboss = True )
-                ic.template_event_from_keymap_item(item)
+        ic = it.row(align = True)
+        ic.prop(item.properties , "tool_mode" , text = "" , emboss = True )
 
-                ic = it.row(align = True)
-                ic.prop(item.properties , "tool_mode" , text = "" , emboss = True )
+        if( item.properties.tool_mode == 'LOWPOLY' ) :
+            im = ic.row()
+            im.active = item.properties.is_property_set("geometry_type")
+            im.prop(item.properties, "geometry_type" , text = "" , emboss = True , expand = False , icon_only = False )
 
-#               op = it.popover(panel="VIEW3D_PT_tools_polyquilt_keymap_properties" , text = item.properties.tool_mode )
-#               op.item_id = 0
+        if( item.properties.tool_mode == 'BRUSH' ) :
+            im = ic.row()
+            im.active = item.properties.is_property_set("brush_type")
+            im.prop(item.properties, "brush_type" , text = "" , emboss = True , expand = False , icon_only = False )
 
-                if( item.properties.tool_mode == 'LOWPOLY' ) :
-                    im = ic.row()
-                    im.active = item.properties.is_property_set("geometry_type")
-                    im.prop(item.properties, "geometry_type" , text = "" , emboss = True , expand = False , icon_only = False )
+        if( item.properties.tool_mode == 'LOOPCUT' ) :
+            im = ic.row()
+            im.active = item.properties.is_property_set("loopcut_mode")
+            im.prop(item.properties, "loopcut_mode" , text = "" , emboss = True , expand = False , icon_only = False )
 
-                if( item.properties.tool_mode == 'BRUSH' ) :
-                    im = ic.row()
-                    im.active = item.properties.is_property_set("brush_type")
-                    im.prop(item.properties, "brush_type" , text = "" , emboss = True , expand = False , icon_only = False )
+        if (not item.is_user_defined) and item.is_user_modified:
+            it.operator("preferences.keyitem_restore", text="", icon='BACK').item_id = item.id
+        elif item.is_user_defined :
+            it.operator("preferences.keyitem_remove", text="", icon='X').item_id = item.id
 
-                if( item.properties.tool_mode == 'LOOPCUT' ) :
-                    im = ic.row()
-                    im.active = item.properties.is_property_set("loopcut_mode")
-                    im.prop(item.properties, "loopcut_mode" , text = "" , emboss = True , expand = False , icon_only = False )
-
-
-                if (not item.is_user_defined) and item.is_user_modified:
-                    it.operator("preferences.keyitem_restore", text="", icon='BACK').item_id = item.id
-                elif item.is_user_defined :
-                    it.operator("preferences.keyitem_remove", text="", icon='X').item_id = item.id
-
-#    layout.operator("preferences.keyitem_add", text="Add New", text_ctxt=i18n_contexts.id_windowmanager, icon='ADD')
     layout.operator(PQ_OT_DirtyKeymap.bl_idname)
-#    popover_kw = {"space_type": 'VIEW_3D', "region_type": 'UI', "category": "Tool"}    
-#    op = layout.popover_group(context=".poly_quilt_keymap_properties", **popover_kw)
-#                   if it.active :
-#                      it.context_pointer_set( "brush_type"  , item.properties )
-#                it = layout.column()
-#                        rna_keymap_ui.draw_kmi(
-#                                [], keyconfing, keymap, item, it, 0)
-#                       it.template_keymap_item_properties(item)
-#                            for m in inspect.getmembers(item.properties):
-#                                print(m)
 
 
 def draw_tool_keymap_ui( context , _layout , text , tool) :
